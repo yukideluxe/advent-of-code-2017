8e4eb324a1ea8926f5987e8f3d6f8bd14537f540
Julia López <julialopez@gmail.com>
2017-12-01 09:37:35 -0500

    puzzle 1 part 2

--- 1.rb
+++ 1.rb
@@ -2,8 +2,10 @@ puts "Enter Input"
 input = gets.chomp
 array = input.split('').reverse
 
+distance = array.size / 2
+
 result = array.each_with_index.reduce(0) do |sum, (digit, i)|
-  sum += digit.to_i if digit == array[i - 1]
+  sum += digit.to_i if digit == array[i - distance]
 
   sum
 end
