6e06769f058119bc76f5a78c05d7316899c35b25
Julia López <julialopez@gmail.com>
2017-12-03 21:18:27 -0500

    puzzle 3 part 2

--- 3.rb
+++ 3.rb
@@ -1,3 +1,5 @@
+# PART 1
+
 def find_vector(number)
   return [0, 0] if number == 1
 
@@ -37,8 +39,6 @@ def find_vector(number)
       distance += 1
     end
   end
-
-  return [x, y]
 end
 
 def manhattan_distance(origin, destination)
@@ -48,13 +48,77 @@ def manhattan_distance(origin, destination)
   (origin_vector.first - destination_vector.first).abs + (origin_vector.last - destination_vector.last).abs
 end
 
-puts "Data :: 1"
+puts "Distance to 1"
 puts manhattan_distance(1, 1)
-puts "Data :: 12"
+puts "Distance to 12"
 puts manhattan_distance(12, 1)
-puts "Data :: 23"
+puts "Distance to 23"
 puts manhattan_distance(23, 1)
-puts "Data :: 1024"
+puts "Distance to 1024"
 puts manhattan_distance(1024, 1)
-puts "Data :: 368078"
+puts "Distance to 368078"
 puts manhattan_distance(368078, 1)
+
+## PART 2
+
+def first_larger_number_than(number)
+  current_number = 1
+  x, y = [0, 0]
+  dx, dy = [1, 0]
+
+  values = {
+    [x, y] => current_number
+  }
+
+  distance = 1
+  direction_changes = 0
+
+  while true
+    distance.times do
+      x += dx
+      y += dy
+
+      current_number =
+        values[[x - 1, y]].to_i +
+        values[[x, y - 1]].to_i +
+        values[[x + 1, y]].to_i +
+        values[[x, y + 1]].to_i +
+        values[[x + 1, y + 1]].to_i +
+        values[[x - 1, y - 1]].to_i +
+        values[[x - 1, y + 1]].to_i +
+        values[[x + 1, y - 1]].to_i
+
+      values[[x, y]] = current_number
+
+      if current_number > number
+        return current_number
+      end
+    end
+
+    # change direction
+    direction_changes += 1
+    dx, dy = if [dx, dy] == [1, 0]
+               [0, 1]
+             elsif [dx, dy] == [0, 1]
+               [-1, 0]
+             elsif [dx, dy] == [-1, 0]
+               [0, -1]
+             elsif [dx, dy] == [0, -1]
+               [1, 0]
+             end
+
+    # every two changes of direction the distance increases
+    if direction_changes % 2 == 0
+      distance += 1
+    end
+  end
+end
+
+puts "First larger number than 10"
+puts first_larger_number_than(10)
+
+puts "First larger number than 142"
+puts first_larger_number_than(142)
+
+puts "First larger number than 368078"
+puts first_larger_number_than(368078)
